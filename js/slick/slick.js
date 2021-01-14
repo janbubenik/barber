98943b65b58992021041f6d984c3f634ce7c2201
janbubenik <68082574+janbubenik@users.noreply.github.com>
2021-01-14 17:45:51 +0100

    commit

---
+++ js/slick/slick.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(document).ready(function () {
+    $(".carousel").slick({
+      infinite: true,
+      dots: true,
+      speed: 400,
+      prevArrow: false,
+      nextArrow: false,
+      infinite: true,
+      cssEase: "linear",
+      swipe: true,
+    });
+  });
+})(jQuery);
